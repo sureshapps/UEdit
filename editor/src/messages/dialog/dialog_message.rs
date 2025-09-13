08f308c831effe07a3c20c58391151b1c811be8a
Suresh Kaleyannan <hello@suresh.app>
2025-09-13 15:53:41 +0800

    sureshks

---
+++ editor/src/messages/dialog/dialog_message.rs
@@ -0,0 +1,41 @@
+use crate::messages::prelude::*;
+
+#[impl_message(Message, Dialog)]
+#[derive(PartialEq, Clone, Debug, serde::Serialize, serde::Deserialize)]
+pub enum DialogMessage {
+	// Sub-messages
+	#[child]
+	ExportDialog(ExportDialogMessage),
+	#[child]
+	NewDocumentDialog(NewDocumentDialogMessage),
+	#[child]
+	PreferencesDialog(PreferencesDialogMessage),
+
+	// Messages
+	CloseAllDocumentsWithConfirmation,
+	CloseDialogAndThen {
+		followups: Vec<Message>,
+	},
+	DisplayDialogError {
+		title: String,
+		description: String,
+	},
+	RequestAboutGraphiteDialog,
+	RequestAboutGraphiteDialogWithLocalizedCommitDate {
+		localized_commit_date: String,
+		localized_commit_year: String,
+	},
+	RequestComingSoonDialog {
+		issue: Option<u32>,
+	},
+	RequestDemoArtworkDialog,
+	RequestExportDialog,
+	RequestLicensesDialogWithLocalizedCommitDate {
+		localized_commit_year: String,
+	},
+	RequestLicensesThirdPartyDialogWithLicenseText {
+		license_text: String,
+	},
+	RequestNewDocumentDialog,
+	RequestPreferencesDialog,
+}
