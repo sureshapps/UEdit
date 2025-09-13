a97ecec117d28b094e3d5ca0a4260ce16ab26125
Suresh Kaleyannan <hello@suresh.app>
2025-09-13 15:53:09 +0800

    sureshks

---
+++ editor/src/messages/preferences/preferences_message.rs
@@ -0,0 +1,19 @@
+use crate::messages::portfolio::document::utility_types::wires::GraphWireStyle;
+use crate::messages::preferences::SelectionMode;
+use crate::messages::prelude::*;
+
+#[impl_message(Message, Preferences)]
+#[derive(PartialEq, Clone, Debug, serde::Serialize, serde::Deserialize)]
+pub enum PreferencesMessage {
+	// Management messages
+	Load { preferences: PreferencesMessageHandler },
+	ResetToDefaults,
+
+	// Per-preference messages
+	UseVello { use_vello: bool },
+	SelectionMode { selection_mode: SelectionMode },
+	VectorMeshes { enabled: bool },
+	ModifyLayout { zoom_with_scroll: bool },
+	GraphWireStyle { style: GraphWireStyle },
+	ViewportZoomWheelRate { rate: f64 },
+}
