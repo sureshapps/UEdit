eef4bc843762e1186886e21e75b5b31eca25aa73
Suresh Kaleyannan <hello@suresh.app>
2025-09-13 15:53:54 +0800

    sureshks

---
+++ editor/src/messages/portfolio/mod.rs
@@ -0,0 +1,12 @@
+mod portfolio_message;
+mod portfolio_message_handler;
+
+pub mod document;
+pub mod document_migration;
+pub mod menu_bar;
+pub mod utility_types;
+
+#[doc(inline)]
+pub use portfolio_message::{PortfolioMessage, PortfolioMessageDiscriminant};
+#[doc(inline)]
+pub use portfolio_message_handler::{PortfolioMessageContext, PortfolioMessageHandler};
