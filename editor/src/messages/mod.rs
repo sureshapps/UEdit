589d9519428f7e87133fb4d1aad14330a64e7fcd
Suresh Kaleyannan <hello@suresh.app>
2025-09-13 15:52:50 +0800

    sureshks

---
+++ editor/src/messages/mod.rs
@@ -0,0 +1,18 @@
+//! The root-level messages forming the first layer of the message system architecture.
+
+pub mod animation;
+pub mod app_window;
+pub mod broadcast;
+pub mod debug;
+pub mod defer;
+pub mod dialog;
+pub mod frontend;
+pub mod globals;
+pub mod input_mapper;
+pub mod input_preprocessor;
+pub mod layout;
+pub mod message;
+pub mod portfolio;
+pub mod preferences;
+pub mod prelude;
+pub mod tool;
