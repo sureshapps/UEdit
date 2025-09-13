6d2fa1b9ca9579ec14b72875be9c3cab2bca5799
Suresh Kaleyannan <hello@suresh.app>
2025-09-13 16:04:56 +0800

    sureshks

---
+++ frontend/src/components/window/MainWindow.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import type { AppWindowPlatform } from "@graphite/messages";
+
+	import LayoutCol from "@graphite/components/layout/LayoutCol.svelte";
+	import StatusBar from "@graphite/components/window/status-bar/StatusBar.svelte";
+	import TitleBar from "@graphite/components/window/title-bar/TitleBar.svelte";
+	import Workspace from "@graphite/components/window/workspace/Workspace.svelte";
+
+	export let platform: AppWindowPlatform;
+	export let maximized: boolean;
+	export let viewportHolePunch: boolean;
+</script>
+
+<LayoutCol class="main-window" classes={{ "viewport-hole-punch": viewportHolePunch }}>
+	<TitleBar {platform} {maximized} />
+
+	<Workspace />
+
+	<StatusBar />
+</LayoutCol>
+
+<style lang="scss" global>
+	.main-window {
+		height: 100%;
+		overflow: auto;
+		touch-action: none;
+	}
+</style>
